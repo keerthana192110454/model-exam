2d5293d41bb7256b36967d78cb59887f45c8f80c
keerthana192110454 <112535982+keerthana192110454@users.noreply.github.com>
2022-09-16 11:54:34 +0530

    test cases
    0
    -1254
    A144
    145.23
    23.456

---
+++ modelexam ques1.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	int num,i=1;
+	while(i=1)
+{
+	printf("\n enter the number:\n");
+	scanf("%f",&num);
+	if(num!=0)
+	{
+
+	if(num%2==0)
+	{
+		printf("\n the given number is even");
+	}
+	else
+	{
+		printf("the given number is odd");
+	}
+}
+	else
+{
+	printf("invalid");
+}
+    printf("\n to continue press 1,else press 0:");
+	scanf("%f",&i);
+}
+	
+}
